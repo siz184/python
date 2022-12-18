b540fb7efa878d409b0b47eedca853cdc0951489
Andrey <siz184@ya.ru>
2022-12-19 01:46:06 +0300

    ht3 - 5

---
+++ less3/ex5.py
@@ -0,0 +1,21 @@
+def fibo(num):
+    if num==0 or num==1:
+        return num
+    return fibo(num-1)+fibo(num-2)
+
+def negafibo(num):
+    return(-1)**(num-1)*fibo(num)
+
+list = []
+n = 10
+
+for i in range(n+1):
+    list.append(negafibo(i))
+
+list.reverse()
+list.pop(-1)
+
+for i in range(n+1):
+    list.append(fibo(i))
+
+print(list)
