c5b7d0c1951a0aa3a9fcf50856cf4a07bc70e714
Andrey <siz184@ya.ru>
2022-12-22 17:50:27 +0300

    ht4 - 4

---
+++ less4/ex4.py
@@ -0,0 +1,21 @@
+import random
+k = 5
+c = [random.randint(-100,100) for i in range(0,k+1)]
+print(c)
+
+def Pol(k,c):
+    p = ''
+    for i in range(0, k+1):
+        if c[i] == 0:
+            pol += ''  
+        elif k - i > 1:
+            p += f'{c[i]} * x^{k - i} + '
+        elif k - i  == 1:
+            p += f'{c[i]} * x + '   
+        else:
+            p+= f'{c[i]} = 0'
+    return p
+
+with open ('p.txt', 'w') as data:
+    data.write(Pol(k,c).replace('+ -','- '))
+
