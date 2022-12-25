bb2360c764e4932d9d435f9b9910e6ff80372618
Andrey <siz184@ya.ru>
2022-12-26 02:18:16 +0300

    ht5 - 2

--- less5/ex2.py
+++ less5/ex2.py
@@ -1,18 +1,63 @@
 positions = [
-    '-','-','-', #1,2,3
-    '-','-','-', #4,5,6
-    '-','-','-'  #7,8,9
+    '-','-','-', #1,2,3 for users / 0,1,2
+    '-','-','-', #4,5,6 for users / 3,4,5
+    '-','-','-'  #7,8,9 for users / 6,7,8
 ]
+
+# positions = [
+#     '-','-','-','-', #1,2,3,4 for users     / 0,1,2,3
+#     '-','-','-','-', #5,6,7,8 for users     / 4,5,6,7
+#     '-','-','-','-', #9,10,11,12 for users  / 8,9,10,11
+#     '-','-','-','-'  #13,14,15,16 for users / 12,13,14,15
+# ]
+# c = 4
+
+winner = 0
 signs = ['-','x','o']
 turn = 1
+c = 3
 
 def checkWin(positions,sign):
     check=0
     a=0
-    b=3
-    for i in range(a,b):
+    b=c
+    for i in range(c):
+        check = 0
+        for j in range(a,b):
+            if positions[j]==sign:
+                check+=1
+            if check==c:
+                return 1    
+        a+=c
+        b+=c
+
+    a=0
+    b=c**2-c+1
+    for i in range(c):
+        check = 0
+        for j in range(a,b,c):
+            if positions[j]==sign:
+                check+=1
+            if check==c:
+                return 1  
+        a+=1
+        b+=1
+
+    check = 0
+    for i in range(0,c**2,c+1):
         if positions[i]==sign:
             check+=1
+        if check==c:
+            return 1
+
+    check = 0
+    for i in range(c-1,c**2-c+1,c-1):
+        if positions[i]==sign:
+            check+=1
+        if check==c:
+            return 1 
+
+    return 0
 
 
 def checkEnd(positions):
@@ -29,30 +74,37 @@ def checkFill(positions,pos):
 
 def oneTurn(positions,turn):
     player = int(input(f"Player {turn}: "))
-    if 0 < player > 9:
+    if 0 < player > c**2:
         print ("Fill 1 - 9")
     elif checkFill(positions,player - 1):
         print (f"Position {player} is filled")
     else:
         positions[player - 1] = signs[turn]
-        turn += 1
-        turn = 1 if turn > 2 else 2
-    return positions,turn
+        if checkWin(positions,signs[turn]):
+            return positions,turn,1
+        else:    
+            turn += 1
+            turn = 1 if turn > 2 else 2
+    return positions,turn,0
 
 def createTable(positions):
     row = ''
     for i in range(0,len(positions)):
         p = i+1
         row += positions[i] + ' '
-        if p%3==0 :
+        if p%c==0 :
             print(f"{row}")
             row = ''
 
 while checkEnd(positions) != 1:
-    positions,turn = oneTurn(positions,turn)
-    createTable(positions)
-        
+    if winner==1:
+        break
+    else:
+        positions,turn,winner = oneTurn(positions,turn)
+        createTable(positions)
     
     
-
-print(f"Player {turn} win!")
+if winner==1:
+    print(f"Player {turn} win!")
+else:
+    print("Draw!")
